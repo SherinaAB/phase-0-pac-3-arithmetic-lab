e1b32a8f341bab2cf15719069c684a9679990304
Sherina Buenaseda <sherina76@gmail.com>
2023-04-25 02:57:22 -0400

    complete arithmetic lab

---
+++ index.js
@@ -0,0 +1,61 @@
+function add() {
+
+}
+
+function subtract() {
+   
+}
+
+function multiply() {
+
+}
+
+function divide() {
+    
+}
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+  
+function multiply(a,b) {
+    return a * b;
+  }
+  
+function divide(a,b) {
+    return a / b;
+}
+
+let number = 10;
+function increment(n){        
+    return (n + 1);
+} 
+
+let n = 10;
+function decrement(n){        
+    return (n - 1);
+} 
+
+function makeInt(n){
+    return parseInt(n)
+    }
+
+function assumeBase10(n){
+    return parseInt(0,10)
+    }
+
+function makeInt(n) {
+    return parseInt(n,10)
+    }
+
+function preserveDecimal(n) {
+    return parseFloat("2.222");
+    }
+
+function preserveDecimal(n) {
+    return parseFloat(n,10);
+    }
